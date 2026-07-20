e2ead75b2c54f0df866261291157e06f95e1fe62
enskyeing <skyetsatsis@gmail.com>
2026-07-19 22:33:11 -0700

    Create basic app layout

--- src/pomodorotimer.py
+++ src/pomodorotimer.py
@@ -1,3 +1,5 @@
+from logging import root
+
 import customtkinter
 
 
@@ -6,14 +8,47 @@ class PomodoroTimerApp(customtkinter.CTk):
         super().__init__()
         self.geometry("400x400")
 
-        self.settings_btn = customtkinter.CTkButton(self, width=20, height=20, command=self.settings_button_callback)
-        self.play_pause_btn = customtkinter.CTkButton(self, width=60, height=20, command=self.play_pause_button_callback)
-        self.reset_btn = customtkinter.CTkButton(self, width=60, height=20, command=self.reset_button_callback)
-    
+        self.settings_btn = customtkinter.CTkButton(self, width=20, height=20, text="⚙️", command=self.settings_button_callback)
+
+        self.btn_frame = customtkinter.CTkFrame(self, width=100, height=50)
+        self.play_btn = customtkinter.CTkButton(self.btn_frame, width=20, height=20, text="▶️", command=self.play_button_callback)
+        self.pause_btn = customtkinter.CTkButton(self.btn_frame, width=20, height=20, text="⏸️", command=self.pause_button_callback)
+        self.stop_btn = customtkinter.CTkButton(self.btn_frame, width=20, height=20, text="⏹️", command=self.stop_button_callback)
+        self.reset_btn = customtkinter.CTkButton(self.btn_frame, width=20, height=20, text="🔄", command=self.reset_button_callback)
+
+        self.timer_frame = customtkinter.CTkFrame(self, width=100, height=50)
+        self.timer_text = customtkinter.CTkLabel(self.timer_frame, text="25:00", font=("Arial", 24))
+
+        self.rs_spacer_frame = customtkinter.CTkFrame(self, width=20, height=20)
+
+        # Build layout
+        self.settings_btn.grid(row=0, column=0, padx=10, pady=10)
+
+        self.timer_frame.grid(row=1, column=1, columnspan=6, padx=10, pady=10)
+        self.timer_text.grid(row=0, column=0)
+
+        self.btn_frame.grid(row=2, column=2, columnspan=4, padx=10, pady=10)
+        self.reset_btn.grid(row=0, column=0, padx=5, pady=5)
+        self.play_btn.grid(row=0, column=1, padx=5, pady=5)
+        self.pause_btn.grid(row=0, column=2, padx=5, pady=5)
+        self.stop_btn.grid(row=0, column=3, padx=5, pady=5)
+
+        self.rs_spacer_frame.grid(row=0, column=7, padx=10, pady=10)
+
+        # Make columns expand equally
+        for i in range(7):
+            self.columnconfigure(i, weight=1)
+
     def settings_button_callback(self):
         pass
 
-    def play_pause_button_callback(self):
+    def play_button_callback(self):
+        pass
+
+    def pause_button_callback(self):
+        pass
+
+    def stop_button_callback(self):
         pass
 
     def reset_button_callback(self):
